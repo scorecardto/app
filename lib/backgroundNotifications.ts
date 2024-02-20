4bf1ad3526588762f662c9ed547fc9078cf55c9e
Malcolm Roalson <mtroalson@gmail.com>
2024-02-20 11:39:52 -0600

    allow batch or single check for registered notifications

--- lib/backgroundNotifications.ts
+++ lib/backgroundNotifications.ts
@@ -132,13 +132,16 @@ async function getExpoToken() {
 let token: string | undefined;
 
 // the actual result will be in `response.data.result`
-export function isRegisteredForNotifs(courseIds: string[]) {
-    return axios.post("https://scorecardgrades.com/api/notifications", {
+export function isRegisteredForNotifs(courseId: string|string[]) {
+    let body = {
         method: 'isRegistered',
         fcmToken,
         expoPushToken: token,
-        courseIds
-    });
+    };
+    // @ts-ignore
+    body[typeof(courseId) === 'string' ? "courseId" : "courseIds"] = courseId;
+
+    return axios.post("https://scorecardgrades.com/api/notifications", body);
 }
 export function registerNotifs(courseId: string, courseName?: string, onetime?: boolean) {
     return axios.post("https://scorecardgrades.com/api/notifications", {
