59c64da13b93b266abf63a4e2753a04bdcd62661
Malcolm Roalson <mtroalson@gmail.com>
2024-08-15 18:15:41 -0500

    fix alert fetcher

--- components/core/AlertFetcher.tsx
+++ components/core/AlertFetcher.tsx
@@ -2,21 +2,26 @@ import React, {useContext} from "react";
 import BottomSheetContext from "../util/BottomSheet/BottomSheetContext";
 import firestore from "@react-native-firebase/firestore";
 import ScorecardModule from "../../lib/expoModuleBridge";
-import WebView from "react-native-webview";
+import {BottomSheetView} from "@gorhom/bottom-sheet";
+import {Dimensions, View} from "react-native";
+import BottomSheetHeader from "../util/BottomSheet/BottomSheetHeader";
+import RenderHTML from "react-native-render-html";
 
 export default function AlertFetcher(): undefined {
     const sheets = useContext(BottomSheetContext);
 
     firestore().collection("alerts").get().then(snapshot => {
-        const seen = JSON.parse(ScorecardModule.getItem("seenAlerts"))
+        const seen = JSON.parse(ScorecardModule.getItem("seenAlerts")) ?? [];
 
         for (const doc of snapshot.docs) {
             if (!seen.includes(doc.id)) {
-                sheets?.addSheet(s => <WebView
-                    originWhitelist={['*']}
+                seen.push(doc.id)
+                sheets?.addSheet(p => <BottomSheetView><RenderHTML
+                    contentWidth={Dimensions.window().width}
                     source={{html: doc.get<string>("html")}}
-                />)
+                /></BottomSheetView>)
             }
         }
+        ScorecardModule.storeItem("seenAlerts", JSON.stringify(seen))
     })
 }
