bb7179ef540ebd6244282ab4294b8a78a31ebe17
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2024-08-16 11:54:35 -0500

    auto refresh

--- components/screens/CourseScreen.tsx
+++ components/screens/CourseScreen.tsx
@@ -1,5 +1,5 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../core/state/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../core/state/store";
 import { View } from "react-native";
 import CourseScreenWrapper from "../app/course/CourseScreenWrapper";
 import useColors from "../core/theme/useColors";
@@ -8,21 +8,173 @@ import CourseScreenGradient from "../app/course/CourseScreenGradient";
 import LargeText from "../text/LargeText";
 import CourseAverageDisplay from "../app/course/CourseAverageDisplay";
 import GradebookWrapper from "../app/gradebook/GradebookWrapper";
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { NavigationProp } from "@react-navigation/native";
+import Storage from "expo-storage";
+import Toast from "react-native-toast-message";
+import {
+  fetchGradeCategoriesForCourse,
+  fetchGradeCategoriesForCourses,
+  fetchReportCard,
+} from "../../lib/fetcher";
+import {
+  setRefreshStatus,
+  setRSType,
+} from "../core/state/grades/refreshStatusSlice";
+import { AllCoursesResponse } from "scorecard-types";
 export default function CourseScreen(props: {
   route: any;
   navigation: NavigationProp<any>;
 }) {
   const { key } = props.route.params;
 
-  const course = useSelector((r: RootState) => {
+  const courseInitial = useSelector((r: RootState) => {
     return r.gradeData.record?.courses.find((c) => c.key === key);
   });
 
-  const average = useSelector((r: RootState) => {
-    return course?.grades[r.gradeCategory.category]?.value;
-  });
+  const login = useSelector((state: RootState) => state.login);
+
+  const gradeCategory =
+    props.route.params.gradeCategory ??
+    useSelector((root: RootState) => root.gradeCategory.category);
+
+  const recordGrade = useSelector(
+    (root: RootState) => root.gradeData.record?.gradeCategory
+  );
+  const [course, setCourse] = useState(
+    gradeCategory === recordGrade ? courseInitial : null
+  );
+
+  const dispatch = useDispatch<AppDispatch>();
+  const refreshGradingPeriod = useCallback(
+    async (allowGetFromStorage: boolean) => {
+      // setLastUpdatedOldGradingPeriod(undefined);
+
+      const oldGradebooks = JSON.parse(
+        ((await Storage.getItem({ key: "oldGradebooks" })) || "{}") as string
+      );
+
+      const alternateKey = courseInitial?.grades[gradeCategory]?.key;
+
+      if (courseInitial == null || alternateKey == null) return;
+
+      if (oldGradebooks[alternateKey] != null && allowGetFromStorage) {
+        if (!courseInitial.grades[gradeCategory]?.active) {
+          setCourse(oldGradebooks[alternateKey]);
+          // setGradeText(
+          //   oldGradebooks[alternateKey].grades[gradeCategory]?.value || "NG"
+          // );
+          // setLastUpdatedOldGradingPeriod(
+          //   oldGradebooks[alternateKey].lastUpdated
+          // );
+          return;
+        } else {
+          Toast.show({
+            type: "info",
+            text1: "Refreshing Grades",
+            text2:
+              "Scorecard has a copy of this grading period, but is refreshing from Frontline since grades are still active.",
+          });
+        }
+      }
+
+      dispatch(
+        setRefreshStatus({
+          type: "GETTING_COURSES",
+          status: "Downloading old data...",
+          taskRemaining: 1,
+          tasksCompleted: 3,
+        })
+      );
+
+      const reportCard = await fetchReportCard(
+        login.district,
+        login.username,
+        login.password,
+        () => {},
+        (status) => {
+          // setRefreshStatus(status);
+        }
+      );
+
+      dispatch(
+        setRefreshStatus({
+          type: "GETTING_COURSES",
+          status: "Loading course data...",
+          taskRemaining: 2,
+          tasksCompleted: 3,
+        })
+      );
+
+      const categories = await fetchGradeCategoriesForCourse(
+        login.district,
+        reportCard.sessionId,
+        reportCard.referer,
+        {
+          ...courseInitial,
+          key: alternateKey || "",
+        }
+      );
+
+      dispatch(setRSType("IDLE"));
+
+      setCourse({
+        ...courseInitial,
+        gradeCategories: categories.gradeCategories,
+      });
+
+      // setGradeText(courseInitial.grades[gradeCategory]?.value || "NG");
+
+      // setLastUpdatedOldGradingPeriod(new Date().toISOString());
+
+      oldGradebooks[alternateKey] = {
+        ...courseInitial,
+        gradeCategories: categories.gradeCategories,
+        lastUpdated: new Date().toISOString(),
+      };
+
+      Storage.setItem({
+        key: "oldGradebooks",
+        value: JSON.stringify(oldGradebooks),
+      });
+
+      fetchGradeCategoriesForCourses(
+        login.district,
+        {
+          ...reportCard,
+          courses: reportCard.courses.map((c) => {
+            return {
+              ...c,
+              key: c.grades[gradeCategory]?.key || c.key,
+            };
+          }),
+        },
+        () => {},
+        gradeCategory
+      ).then(async (categories: AllCoursesResponse) => {
+        const oldGradebooks = JSON.parse(
+          ((await Storage.getItem({ key: "oldGradebooks" })) || "{}") as string
+        );
+
+        categories.courses.forEach((c) => {
+          oldGradebooks[c.key] = {
+            ...c,
+            lastUpdated: new Date().toISOString(),
+          };
+        });
+
+        Storage.setItem({
+          key: "oldGradebooks",
+          value: JSON.stringify(oldGradebooks),
+        });
+      });
+    },
+    [courseInitial, gradeCategory, login]
+  );
+
+  const average = useMemo(() => {
+    return course?.grades[gradeCategory]?.value;
+  }, [gradeCategory, course]);
 
   const grades = useSelector((r: RootState) => {
     return course?.gradeCategories;
@@ -37,6 +189,29 @@ export default function CourseScreen(props: {
   const [modifiedAvg, setModifiedAvg] = useState<string | null>(null);
   const [resetKey, setResetKey] = useState(0);
 
+  useEffect(() => {
+    if (course != null) return;
+
+    console.log("Refreshing");
+
+    refreshGradingPeriod(true);
+  }, [courseInitial]);
+
+  useEffect(() => {
+    if (course == null) return;
+
+    // if (gradeChangeTable.changed) {
+    //   setTimeout(() => {
+    //     dispatch(
+    //       setOldCourseState({
+    //         key: key,
+    //         value: captureCourseState(course),
+    //         save: "STATE_AND_STORAGE",
+    //       })
+    //     );
+    //   }, 1500);
+    // }
+  }, []);
   return (
     <CourseScreenWrapper courseKey={key}>
       <View
--- components/screens/CurrentGradesScreen.tsx
+++ components/screens/CurrentGradesScreen.tsx
@@ -1,7 +1,7 @@
 import { NavigationProp } from "@react-navigation/native";
 
-import { ScrollView, View } from "react-native";
-import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../core/state/store";
 import { FlatList } from "react-native-gesture-handler";
@@ -10,6 +10,12 @@ import { Course } from "scorecard-types";
 import PageThemeProvider from "../core/context/PageThemeProvider";
 import Background from "../util/Background";
 import DashboardToolbar from "../app/dashboard/DashboardToolbar";
+import StatusText from "../text/StatusText";
+import useColors from "../core/theme/useColors";
+import { fetchAllContent } from "../../lib/fetcher";
+import RefreshStatus from "../../lib/types/RefreshStatus";
+import { setRefreshStatus } from "../core/state/grades/refreshStatusSlice";
+import fetchAndStore from "../../lib/fetchAndStore";
 export default function CurrentGradesScreen(props: {
   navigation: NavigationProp<any>;
 }) {
@@ -27,6 +33,90 @@ export default function CurrentGradesScreen(props: {
     (s: RootState) => s.gradeCategory.category
   );
 
+  const lastUpdated = useSelector(
+    (state: RootState) => state.gradeData.record?.date ?? 0
+  );
+
+  const [time, setTime] = useState(Date.now());
+  const lastUpdatedText = useMemo(() => {
+    if (time - lastUpdated < 1000 * 60 * 60) {
+      const mins = Math.floor((time - lastUpdated) / 1000 / 60);
+
+      if (mins <= 0) return `Up to date.`;
+      return `Updated ${mins} minute${mins === 1 ? "" : "s"} ago`;
+    }
+
+    if (time - lastUpdated < 1000 * 60 * 60 * 24) {
+      return `Updated ${Math.floor(
+        (time - lastUpdated) / 1000 / 60 / 60
+      )} hours ago`;
+    }
+
+    if (time - lastUpdated < 1000 * 60 * 60 * 24 * 2) {
+      return "Updated yesterday";
+    }
+
+    if (time - lastUpdated < 1000 * 60 * 60 * 24 * 7) {
+      return `Updated ${Math.floor(
+        (time - lastUpdated) / 1000 / 60 / 60 / 24
+      )} days ago`;
+    }
+
+    return `Updated on ${new Date(lastUpdated).toLocaleDateString()}`;
+  }, [currentGradeCategory, lastUpdated, time]);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      setTime(Date.now());
+    }, 1000);
+
+    return () => {
+      clearInterval(i);
+    };
+  }, []);
+
+  const district = useSelector((state: RootState) => state.login.district);
+  const username = useSelector((state: RootState) => state.login.username);
+  const password = useSelector((state: RootState) => state.login.password);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    const reportCard = fetchAllContent(
+      district,
+      username,
+      password,
+      undefined,
+      (s: RefreshStatus) => {
+        dispatch(setRefreshStatus(s));
+      }
+    );
+
+    reportCard.then(async (data) => {
+      await fetchAndStore(data, dispatch, false);
+      setRefreshing(false);
+    });
+  }, []);
+
+  const MINS_TO_REFRESH = 60;
+
+  const lastRecordDate = useSelector(
+    (state: RootState) => state.gradeData.record?.date
+  );
+
+  useEffect(() => {
+    if (refreshing || !lastRecordDate) return;
+
+    const mins = Math.floor((Date.now() - lastRecordDate) / 1000 / 60);
+
+    if (mins > MINS_TO_REFRESH) {
+      onRefresh();
+      console.log("will refresh");
+    }
+  }, [lastRecordDate, refreshing]);
+
+  const colors = useColors();
   return (
     <PageThemeProvider
       theme={{
@@ -64,6 +154,22 @@ export default function CurrentGradesScreen(props: {
               keyExtractor={(item) => item.key}
             />
           )}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              width: "100%",
+            }}
+          >
+            <StatusText
+              style={{
+                color: colors.text,
+                fontSize: 14,
+              }}
+            >
+              {lastUpdatedText}
+            </StatusText>
+          </View>
         </ScrollView>
       </Background>
     </PageThemeProvider>
